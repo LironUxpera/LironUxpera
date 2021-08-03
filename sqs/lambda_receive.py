4a82f5ab53705884df552d13ef7298bd3941c14f
Harry Hornreich <harryhorn@gmail.com>
2021-08-03 07:44:16 +0300

    added lambda receive messages code

---
+++ sqs/lambda_receive.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+import urllib3
+import json
+
+def lambda_handler(event, context):
+    for record in event['Records']:
+        print("test")
+        payload = record["body"]
+        print(str(payload))
+        
+        http = urllib3.PoolManager()
+        encoded_body = json.dumps(record)
+        r = http.request('POST', 'http://54.161.16.58:80/event',
+            headers={'Content-Type': 'application/json'},
+            body=encoded_body)
+
